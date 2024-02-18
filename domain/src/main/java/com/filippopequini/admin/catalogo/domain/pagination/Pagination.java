badde00e6c01d6680eaaed28babb8996291d4481
Filippo Pequini <filippo_pequini@hotmail.com>
2024-02-18 13:17:45 -0300

    feat: pagination

---
+++ domain/src/main/java/com/filippopequini/admin/catalogo/domain/pagination/Pagination.java
@@ -0,0 +1,11 @@
+package com.filippopequini.admin.catalogo.domain.pagination;
+
+import java.util.List;
+
+public record Pagination<T>(
+        int currentPage,
+        int perPage,
+        long total,
+        List<T> items
+) {
+}
